Fixed GAT and GraphSage, attempted back edges, added gradient clipping and included DGMRF

dissGNN/data_processing.py
@@ -34,7 +34,8 @@ def convert_level_mappings_to_edges(mappings_path):
     mappings = pd.read_csv(mappings_path)
     edges = mappings[['ADM2_PCODE', 'ADM3_PCODE']]
     edges = edges.rename(columns={'ADM2_PCODE': 'source', 'ADM3_PCODE': 'target'})
-
+    # edges[["source", "target"]] = edges[["target", "source"]] #Used for producing back-edges
+    print("Mappings: ", edges.to_string())
     return edges
 
 
@@ -50,10 +51,8 @@ def load_data(shape_path_coarse, shape_path_fine, features_path_coarse, features
     edges = pd.concat([edges_coarse, edges_fine, edges_mappings])
 
     #Add weights for edges
-    weights = {"coarse" : 0.5, "fine" : 1, "mappings" : 2}
+    weights = {"coarse" : 1, "fine" : 1, "mappings" : 2}
     edges["weights_init"] = edges["type"].map(weights)
-    print(edges)
-
     #Normalize weights TODO
 
     #Load node fgeatures for coarse admin and fine admin level and rename to generealize for concat
@@ -62,16 +61,13 @@ def load_data(shape_path_coarse, shape_path_fine, features_path_coarse, features
     
     node_features_coarse = pd.read_csv(features_path_coarse)
     node_features_coarse = node_features_coarse.rename(columns={col_coarse_pcode : "ADM_PCODE", col_coarse_pt : "ADM_PT"})
-    print(len(node_features_coarse["ADM_PT"]))
 
     col_fine_pcode = "ADM" + str(admin_level_fine) + "_PCODE"
     col_fine_pt = "ADM" + str(admin_level_fine) + "_PT"
     node_features_fine = pd.read_csv(features_path_fine)
     node_features_fine = node_features_fine.rename(columns={col_fine_pcode : "ADM_PCODE", col_fine_pt : "ADM_PT"})
-    print(len(node_features_fine["ADM_PT"]))
 
     node_features = pd.concat([node_features_coarse, node_features_fine])
-    print(len(node_features["ADM_PT"]))
     
     # Check if mismatching districts between shape and feature data
     missing_districts = set(edges["source"]).union(set(edges["target"])) - set(node_features["ADM_PCODE"])
@@ -129,12 +125,19 @@ def process_feature_data(node_features, edges):
 
 
     y_feature = node_features["population_density"]
-    x_unscaled = node_features.drop(columns=["population_density", "ADM_PT", "log_population", "T_TL", "district_area"])
+    x_unscaled = node_features.drop(columns=["population_density", "ADM_PT", "log_population", "T_TL"])
+
+    #Convert certain variables into densities (This does nothing)
+    x_unscaled[['building_count', 'building_area', 'osm_traffic', 'osm_transport', 'osm_places', 'osm_pofw', 'osm_pois', 'osm_railways', 'osm_roads']] = x_unscaled[['building_count', 'building_area', 'osm_traffic', 'osm_transport', 'osm_places', 'osm_pofw', 
+                  'osm_pois', 'osm_railways', 'osm_roads']].div(x_unscaled['district_area'], axis=0)
+    
+    #Drop another unneeded column
+    x_unscaled = x_unscaled.drop(columns=['district_area'])
 
     #Log population density
     y_feature = np.log1p(y_feature)
 
-    # Standardize x (features), but NOT y
+    # Standardize x (features)
     scaler = StandardScaler()
     x_features = scaler.fit_transform(x_unscaled)
 

dissGNN/main.py
@@ -25,25 +25,33 @@ if __name__ == "__main__":
     best_mape = float("inf")
     best_params = {}
 
-    for param in hyperparam_combinations:
-        hidden_size, message_passing_count, drop_prob, learning_rate, weight_decay = param
+    # ------------ Hyper parameter search
+    # for param in hyperparam_combinations:
+    #     hidden_size, message_passing_count, drop_prob, learning_rate, weight_decay = param
 
-        model_inst = model.GCN(input_size, output_size, hidden_size, message_passing_count, drop_prob)
+    #     model_inst = model.GCN(input_size, output_size, hidden_size, message_passing_count, drop_prob)
 
-        loss_fn = nn.SmoothL1Loss() 
-        optimizer = torch.optim.Adam(model_inst.parameters(), lr = learning_rate, weight_decay = weight_decay)
-        mae, mape, rmse, r2 = train_validate.train_loop(400, model_inst, data, optimizer, loss_fn)
+    #     loss_fn = nn.SmoothL1Loss() 
+    #     optimizer = torch.optim.Adam(model_inst.parameters(), lr = learning_rate, weight_decay = weight_decay)
+    #     mae, mape, rmse, r2 = train_validate.train_loop(400, model_inst, data, optimizer, loss_fn)
 
-        model_inst.eval()
+    #     model_inst.eval()
 
-        if mape < best_mape:
-            best_mape = mape
-            best_params = params
+    #     if mape < best_mape:
+    #         best_mape = mape
+    #         best_params = params
             
-        print(best_params)
-        print(best_mape)
+    #     print(best_params)
+    #     print(best_mape)
+    # print(best_mape, "\n", best_params)
+    # --------------
     
-    print(best_mape, "\n", best_params)
+    hidden_size, message_passing_count, drop_prob, learning_rate, weight_decay = 32, 6, 0.3,5e-5, 5e-4
 
+    model_inst = model.GAT(input_size, output_size, hidden_size, message_passing_count, drop_prob)
 
+    loss_fn = nn.SmoothL1Loss() 
+    optimizer = torch.optim.Adam(model_inst.parameters(), lr = learning_rate, weight_decay = weight_decay)
+    mae, mape, rmse, r2 = train_validate.train_loop(400, model_inst, data, optimizer, loss_fn)
 
+    model_inst.eval()

dissGNN/model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from torch_geometric.nn import GCNConv, GATConv, SAGEConv
 import torch.nn.functional as F
+from torch_scatter import scatter_add
 import torch
 
 class GraphSage(nn.Module):
@@ -31,7 +32,7 @@ class GraphSage(nn.Module):
         # Normalize edge weights (Optional, can be removed)
         edge_weight = edge_weight / (edge_weight.max() + 1e-6)  
 
-        for conv in self.convs:
+        for conv in self.conv:
             x = F.dropout(x, p=self.drop_prob, training=self.training)
 
             # Aggregate weighted node features using scatter_add
@@ -51,12 +52,12 @@ class GAT(nn.Module):
 
         #Input to hidden layer mapping
         self.conv = nn.ModuleList()
-        self.conv.append(GATConv(input_size, hidden_layer_size, add_self_loops = False)) 
+        self.conv.append(GATConv(input_size, hidden_layer_size, add_self_loops=True)) 
         
         #Hidden layer to hidden layer mapping
         #Add number of hidden layers to match the number of neighbours
         for i in range(message_passing_count - 1):
-            self.conv.append(GATConv(hidden_layer_size, hidden_layer_size, add_self_loops=False))
+            self.conv.append(GATConv(hidden_layer_size, hidden_layer_size, add_self_loops=True))
 
         #Hidden layer to output mapping
         self.linear = nn.Linear(hidden_layer_size, output_size)
@@ -66,7 +67,7 @@ class GAT(nn.Module):
     def forward(self, x, edge_index, edge_weight):
         for layer in self.conv:
             x = F.dropout(x, p=self.drop_prob, training = self.training)
-            x = layer(x, edge_index, edge_weight=edge_weight)          
+            x = layer(x, edge_index, edge_attr=edge_weight)          
             # x = F.leaky_relu(x, negative_slope = 0.01)
             x = F.relu6(x)
 
@@ -80,12 +81,12 @@ class GCN(nn.Module):
 
         #Input to hidden layer mapping
         self.conv = nn.ModuleList()
-        self.conv.append(GCNConv(input_size, hidden_layer_size, add_self_loops = False)) 
+        self.conv.append(GCNConv(input_size, hidden_layer_size, add_self_loops = True)) 
         
         #Hidden layer to hidden layer mapping
         #Add number of hidden layers to match the number of neighbours
         for i in range(message_passing_count - 1):
-            self.conv.append(GCNConv(hidden_layer_size, hidden_layer_size, add_self_loops=False))
+            self.conv.append(GCNConv(hidden_layer_size, hidden_layer_size, add_self_loops=True))
 
         #Hidden layer to output mapping
         self.linear = nn.Linear(hidden_layer_size, output_size)
@@ -93,10 +94,11 @@ class GCN(nn.Module):
 
     def forward(self, x, edge_index, edge_weight):
         for layer in self.conv:
-            x = F.dropout(x, p=self.drop_prob, training = self.training)
-            x = layer(x, edge_index, edge_weight=edge_weight)          
+            
+            x = layer(x, edge_index, edge_weight=edge_weight)      
+            x = F.dropout(x, p=self.drop_prob, training = self.training)    
             # x = F.leaky_relu(x, negative_slope = 0.01)
-            x = F.relu6(x)
+            x = F.relu(x)
 
         x = self.linear(x)
 

dissGNN/train_validate.py
@@ -5,47 +5,48 @@ def train(model, optimizer, data, loss_fn):
     model.train()
     optimizer.zero_grad()
     output = model(data.x, data.edge_index, data.edge_weight)
+
     loss = loss_fn(torch.expm1(output[data.train_mask].squeeze()), torch.expm1(data.y[data.train_mask]))
     loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), 0.8)
     optimizer.step()
     return loss.item()
 
-def validate(model, data, e=1e-6):
+def validate(model, data, mask, e=1e-6):
+    model.eval()
 
-    with torch.no_grad():
+    output = model(data.x, data.edge_index, data.edge_weight).squeeze()
 
-        output = model(data.x, data.edge_index, data.edge_weight).squeeze()
+    # Compute MAPE
+    actual = torch.expm1(data.y[mask])  # Convert log values back to original scale
+    # actual = scaler.inverse_transform(actual)
+    predicted = torch.expm1(output[mask])  # Convert log predictions back to original scale
+    # predicted = scaler.inverse_transform(predicted)
 
+    # Compute MAE
+    mae = torch.abs(predicted - actual).mean().item()
 
-        # Compute MAPE
-        actual = torch.expm1(data.y[data.val_mask])  # Convert log values back to original scale
-        # actual = scaler.inverse_transform(actual)
-        predicted = torch.expm1(output[data.val_mask])  # Convert log predictions back to original scale
-        # predicted = scaler.inverse_transform(predicted)
+    # Compute RMSE
+    mse = torch.mean((predicted - actual) ** 2)
+    rmse = torch.sqrt(mse).item()
 
-        # Compute MAE
-        mae = torch.abs(predicted - actual).mean().item()
+    # Compute MAPE (Avoid division by zero)
+    mape = (torch.abs((predicted - actual) / (actual + e))).mean().item() * 100  
 
-        # Compute RMSE
-        mse = torch.mean((predicted - actual) ** 2)
-        rmse = torch.sqrt(mse).item()
+    # Compute R² (Coefficient of Determination)
+    ss_total = torch.sum((actual - actual.mean()) ** 2)
+    ss_residual = torch.sum((actual - predicted) ** 2)
+    r2 = 1 - (ss_residual / (ss_total + e)).item()  # Add epsilon to avoid div by zero
 
-        # Compute MAPE (Avoid division by zero)
-        mape = (torch.abs((predicted - actual) / (actual + e))).mean().item() * 100  
-
-        # Compute R² (Coefficient of Determination)
-        ss_total = torch.sum((actual - actual.mean()) ** 2)
-        ss_residual = torch.sum((actual - predicted) ** 2)
-        r2 = 1 - (ss_residual / (ss_total + e)).item()  # Add epsilon to avoid div by zero
-
-    return mae, mape, rmse, r2
+    return mae, mape, rmse, r2  
 
 def train_loop(num_epochs, model, data, optimizer, loss_fn):
     for epoch in range(num_epochs):
         loss = train(model, optimizer, data, loss_fn)
-        mae, mape, rmse, r2 = validate(model, data)
-        
-        print("Epoch Number: ", epoch, ", Loss: ", loss, ", Validation MAE: ", mae, ", Validation MAPE: ", mape, ", Validation RMSE: ", rmse, ", Validation R^2: ", r2, ".")
+        mae, mape, rmse, r2 = validate(model, data, data.val_mask)
+        print("Val - Epoch Number: ", epoch, ", Loss: ", loss, ", MAE: ", mae, ", MAPE: ", mape, ", RMSE: ", rmse, ", R^2: ", r2, ".")
+        mae, mape, rmse, r2 = validate(model, data, data.train_mask)
+        print("Test - Epoch Number: ", epoch, ", Loss: ", loss, ", MAE: ", mae, ", MAPE: ", mape, ", RMSE: ", rmse, ", R^2: ", r2, ".")
     
     return mae, mape, rmse, r2
 